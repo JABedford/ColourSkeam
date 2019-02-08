5c8545b1c7fe00c7361ef66142b08652c16c2439
jabedford <32302360+JABedford@users.noreply.github.com>
2019-02-08 07:36:25 +0000

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+import Header from '../comps/Header';
+
+export default () => (
+    <div>
+        <Header />
+        <p>This is an about page.</p>
+    </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// This is the Link API
+import Link from 'next/link';
+
+import Header from '../comps/Header'
+
+const Index = () => (
+    <div>
+        <Header />
+        <p>Hello Next.js</p>
+    </div>
+)
+
+export default Index
